6d4b057a2c0387cf5ff92a24ac1acdc7c2e13589
eric29200 <eric_begot@yahoo.fr>
2020-03-01 19:01:10 +0100

    remove duplicated struct

--- sort.c
+++ sort.c
@@ -2,36 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "sort.h"
 #include "mem.h"
 
 #define LINE_SIZE	4096
-
-struct line_t {
-	char *value;
-	char *key;
-	size_t key_len;
-};
-
-
-struct chunk_t {
-	FILE *fp;
-	struct line_t **lines;
-	size_t nb_lines;
-	ssize_t size;
-	struct line_t *current_line;
-};
-
-struct data_file_t {
-	char *input_path;
-	char *output_path;
-	struct chunk_t **chunks;
-	size_t nb_chunks;
-	ssize_t chunk_size;
-	char field_delim;
-	int key_field;
-	int header;
-	char header_line[LINE_SIZE];
-};
 
 static struct line_t *line_create(const char *value, char field_delim,
 				  int key_field)
